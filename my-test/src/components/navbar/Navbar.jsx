7fdf928420797aa7f335de81b79bfc7f5f98cea7
tovoRakotoson <tovo.rakotoson@antennereunion.fr>
2023-02-09 15:37:44 +0300

    installation div menu

---
+++ my-test/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        cxcxcxc
+      </div>
+    </>
+  );
+};
+export default Navbar;
--- my-test/src/index.js
+++ my-test/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Layout from './components/layout/Layout';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Layout>
+      <App />
+  </Layout>
 );
 
 // If you want to start measuring performance in your app, pass a function
